eabb90ef63a3590cb1d1c5ca7356c4123b018e66
plxulo <pcfdaa@gmail.com>
2023-03-22 12:24:40 -0300

    added script to the button "limpar" joao says: "amizade magica"

---
+++ js/scriptLimpar.js
@@ -0,0 +1,6 @@
+function limparCampos() {
+    document.getElementById("campo1").value = "";
+    document.getElementById("campo2").value = "";
+    document.getElementById("campo3").value = "";
+    document.getElementById("campo4").value = "";
+}
