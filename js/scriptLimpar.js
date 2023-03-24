df4e30e7f2681ac29a6093927fe5ad3f468fc8a3
Paulo Amarante <87990105+plxulo@users.noreply.github.com>
2023-03-24 08:39:59 -0300

    Update scriptLimpar.js

--- js/scriptLimpar.js
+++ js/scriptLimpar.js
@@ -1,6 +1,7 @@
 const limparButton = document.getElementById("limpar")
 const inputsLimpaveis = [...document.getElementsByClassName("limpavel--botao")];;
 
+//percorrer array para ser escalável
 function limparCampos () {
     inputsLimpaveis.forEach(function (input) {
         input.value = "";
