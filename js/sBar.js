7b0c522c1583909bd113ec64345d5e6e5b9d0a23
plxulo <pcfdaa@gmail.com>
2023-03-30 22:41:57 -0300

    changed the scroll animation and added text

--- js/sBar.js
+++ js/sBar.js
@@ -4,16 +4,20 @@ let lastScroll = 0;
 window.addEventListener("scroll", () => {
     const currentScroll = window.pageYOffset
     
-    if (currentScroll <= 0) {
-        body.classList.remove("scroll-up")
-    }
-    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-up")
-        body.classList.add("scroll-down")
-    }
-    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-        body.classList.remove("scroll-down")
-        body.classList.add("scroll-up")
+    if (currentScroll > 700) {
+
+        if (currentScroll <= 0) {
+            body.classList.remove("scroll-up")
+        }
+        if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+            body.classList.remove("scroll-up")
+            body.classList.add("scroll-down")
+        }
+        if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+            body.classList.remove("scroll-down")
+            body.classList.add("scroll-up")
+        }
+
     }
 
     lastScroll = currentScroll;
