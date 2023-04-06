63880c0a186d0951eee23e6deba29c9ace9e8eb3
plxulo <pcfdaa@gmail.com>
2023-04-05 23:02:46 -0300

    to chegando proximo de um banco de dados

--- php/db.php
+++ php/db.php
@@ -1,5 +1,5 @@
 <?php
-
+    use mysqli;
     namespace Cabuailo;
 
     class gerenciamentoSenha
@@ -31,5 +31,39 @@
 
     echo("<br>");
     echo("Seu email é: " . $user->getMail()); //exibir email
+
+    //acima apenas checagens de funcionamento / recebimento de entradas
+    //abaixo inserção no banco de dados:
+
+    //realizar conexão com o banco de dados, segue comandos admin
+
+    $servername = 'localhost';
+    $username = 'paulo';
+    $password = 'paulo123';
+    $dbname = 'cabuailo';
+
+    //criar a conexão:
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //verificar possiveis erros na conexao:
+
+    if ($conn->connect_error)
+    {
+        die("Erro na conexão: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO usuarios (email, senha)
+            VALUES ('{$user->getMail()}', '{$user->getPassword()}')";
+
+    if ($conn->query($sql) === TRUE)
+    {
+        echo("Usuário cadastrado com sucesso!");
+    }
+    else
+    {
+        echo("Erro ao cadastrar usuário: ". $conn->error);
+    }
+
+    $conn->close();
 
 ?>
