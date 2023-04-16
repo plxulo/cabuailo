7d5a1a496d9b61ca8a1922d079ef0a67ff3d65b0
plxulo <pcfdaa@gmail.com>
2023-04-16 19:22:18 -0300

    animacao loading

--- public/js/preLoader.js
+++ public/js/preLoader.js
@@ -1,5 +1,7 @@
+// Adiciona o pre loader para a página selecionando o elemento de ID "pre_loader"
 var Loader = document.getElementById("pre_loader");
 
+// Ouvinte de eventos na janela que executa uma função alterando o estilo display do ID "pre_loader"
 window.addEventListener("load", function(){
     Loader.style.display = "none";
 })
