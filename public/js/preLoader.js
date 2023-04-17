37c7f3ca303759f5d1f55186f9e38a441e9e381f
plxulo <pcfdaa@gmail.com>
2023-04-16 21:25:05 -0300

    readability

--- public/js/preLoader.js
+++ public/js/preLoader.js
@@ -2,6 +2,19 @@
 var Loader = document.getElementById("pre_loader");
 
 // Ouvinte de eventos na janela que executa uma função alterando o estilo display do ID "pre_loader"
-//window.addEventListener("load", function(){
-    //Loader.style.display = "none";
-//})
+window.addEventListener("load", function(){
+    Loader.style.display = "none";
+})
+
+// ================== ANIMAÇÃO ================== //
+// ================== LOADING ================== //
+
+// Acessa o documento que executa uma função no mover do mouse
+document.addEventListener('mousemove', function(event) {
+var blob = document.getElementById('blob');
+
+blob.animate ({
+    left: event.clientX + 'px', // Posição X do ID "blob"
+    top: event.clientY + 'px'  // Posição Y do ID "blob"
+}, { duration: 3000, fill: "forwards" }); // Parâmetros da animação
+});
