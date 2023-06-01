2eec97cc30f3ac4560bdae4750787fc1efbfe00b
plxulo <pcfdaa@gmail.com>
2023-05-31 23:02:43 -0300

    update

--- php/salvar_imagem.php
+++ php/salvar_imagem.php
@@ -1,17 +1,19 @@
 <?php
+        session_start();
+
         // ATENÇÃO: o tipo da coluna na tabela deve ser MEDIUMBLOB
-        include("conecta.php");
+        include("conecta2.php");
 
         // Lê o conteúdo do arquivo de imagem e armazena na variável $imagem
 		$imagem = file_get_contents($_FILES["imagem"]["tmp_name"]);
 		
-		$comando = $pdo->prepare("INSERT INTO imagem_pfp_admin(imagem) VALUES(:imagem)");
+		$comando = $pdo->prepare("INSERT INTO imagem_pfp_adm(imagem, pfp_adm) VALUES(:imagem, $_SESSION[id])");
         $comando->bindParam(":imagem", $imagem, PDO::PARAM_LOB);
 		$resultado = $comando->execute();
         
         // As linhas abaixo você usará sempre que quiser mostrar a imagem
 
-        $comando = $pdo->prepare("SELECT imagem FROM imagem_pfp_admin");
+        $comando = $pdo->prepare("SELECT imagem FROM imagem_pfp_adm");
 		$resultado = $comando->execute();
         while( $linhas = $comando->fetch() )
         {
