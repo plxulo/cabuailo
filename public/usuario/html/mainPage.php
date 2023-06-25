0ed415b913f46208810441ef1f9553aa97bef7ef
OtavioSantanna <otavio.santana085@gmail.com>
2023-06-25 15:06:56 -0300

    update

--- public/usuario/html/mainPage.php
+++ public/usuario/html/mainPage.php
@@ -5,7 +5,6 @@
     if((!isset($_SESSION['email']) == true ) and (!isset($_SESSION['senha']) == true))
     {
         unset($_SESSION['email']);
-        unset($_SESSION['senha']);
         header('Location: loginUsuario.php');
     }
     $logado = $_SESSION['email'];
